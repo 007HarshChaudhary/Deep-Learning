ef850a6e7ef8a6862abb256a88c82b1966d1dc10
007HarshChaudhary <45226159+007HarshChaudhary@users.noreply.github.com>
2020-05-02 19:05:36 +0530

    Add files via upload

---
+++ AND_GATE.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 26 17:24:24 2020
+
+@author: Harsh Chaudhary
+"""
+
+import torch
+from torch import nn, optim
+
+
+data = torch.Tensor([[0, 0], [0, 1], [1, 0], [1, 1]])
+label = torch.Tensor([[0], [0], [0], [1]])
+
+class AND_GATE(nn.Module):
+    def __init__(self):
+        super(AND_GATE, self).__init__()
+        
+        self.fc1 = nn.Linear(2, 1)
+        #self.fc2 = nn.Linear(3, 1)
+    
+    def forward(self, x):
+        x = torch.sigmoid(self.fc1(x))
+        #x = self.fc2(x)
+        return x
+
+model = AND_GATE()
+criterion = nn.MSELoss()
+optimizer = optim.SGD(model.parameters(), lr = 1)
+
+epochs = 1000
+for e in range(epochs):
+    train_loss = 0
+
+    output = model(data)
+    loss = criterion(output, label)
+    
+    train_loss += loss.item()
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    
+    if e%100==0:
+        print('epoch {}: Training loss: {:.4f}'.format(e+1, train_loss))
+
+def test(a, b):
+    input_values = torch.Tensor([[a, b]])
+    output = model(input_values)
+    print(list(map(fun, output)))
+
+def fun(x):
+    if x>0.5:
+        return 1
+    return 0
